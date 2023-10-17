2f43b2717f2efe91e1323f078bf6cbeed351daa5
dawoodrizwan84 <dawood.rizwan@outlook.com>
2023-10-17 13:42:05 +0200

    Add project files.

---
+++ 23.1News/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _23._1News.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
