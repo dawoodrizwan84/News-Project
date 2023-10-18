43624967198fb6412c27003fd48cb5f953672a0c
Abdoulie Sanyang <abdouliesanyang62@yahoo.com>
2023-10-18 14:11:03 +0200

    Classes/Models completed

---
+++ 23.1News/Models/SubscriptionType.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace _23._1News.Models
+{
+    public class SubscriptionType
+    {
+        public int Id { get; set; }
+        public string TypeName { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ 23.1News/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace _23._1News.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public int DOB { get; set; }
+    }
+}
