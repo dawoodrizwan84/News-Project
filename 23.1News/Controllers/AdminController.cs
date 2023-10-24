15818c5c1a080ebec32ec3f8007390d994b5d98b
Xin Liu <xinliu1108@gmail.com>
2023-10-24 14:56:16 +0200

    fix merge

--- 23.1News/Controllers/AdminController.cs
+++ 23.1News/Controllers/AdminController.cs
@@ -1,24 +1,32 @@
 ﻿using _23._1News.Data;
 using _23._1News.Models;
+using _23._1News.Models.View_Models;
 using _23._1News.Services.Abstract;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace _23._1News.Controllers
 {
-    public class AdminController
+    public class AdminController : Controller
     {
 
         private readonly ILogger<AdminController> _logger;
         private readonly IAdminService _adminService;
+        private readonly IArticleService _articleService;
         public AdminController(ILogger<AdminController> logger, IArticleService articleService, IAdminService adminService)
         {
             _logger = logger;
             _adminService = adminService;
+            _articleService = articleService;
 
         }
 
+       public IActionResult Index()
+  {
+      var articleList = _articleService.GetArticles();
+      return View(articleList);
+  }
 
-        
+
     }
 }
