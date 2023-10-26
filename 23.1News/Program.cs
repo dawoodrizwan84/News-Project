caf15c0d7aac90268c8310027fdc5b111de8dcb5
Xin Liu <xinliu1108@gmail.com>
2023-10-26 13:55:21 +0200

    adminpage

--- 23.1News/Program.cs
+++ 23.1News/Program.cs
@@ -19,7 +19,7 @@ namespace _23._1News
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
