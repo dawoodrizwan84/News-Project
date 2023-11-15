7a519149ea5e0ab125b995ecc4998dbfac76ff6f
Xin Liu <xinliu1108@gmail.com>
2023-11-15 16:10:16 +0100

    subscription

--- 23.1News/Controllers/SubscriptionTypeController.cs
+++ 23.1News/Controllers/SubscriptionTypeController.cs
@@ -2,6 +2,7 @@
 using _23._1News.Models.Db;
 using _23._1News.Models.View_Models;
 using _23._1News.Services.Abstract;
+using _23._1News.Services.Implement;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -36,7 +37,7 @@ namespace _23._1News.Controllers
         }
 
         [HttpPost]
-
+        [ValidateAntiForgeryToken]
         public IActionResult Create(SubscriptionType subscriptionType)
         {
             if (ModelState.IsValid)
@@ -50,6 +51,16 @@ namespace _23._1News.Controllers
         {
             _subscriptionTypeService.DeleteSubscriptionType(id);
             return RedirectToAction("Index");
+        }
+
+        public IActionResult GotoStart(int id)
+        {
+            SubscriptionType subscriptionType = _subscriptionTypeService.GetSubscriptionTypeById(id);
+            TempData["subTypeId"] = subscriptionType.Id;
+            TempData["subTypeName"] = subscriptionType.TypeName;
+            TempData["subTypePrice"] = subscriptionType.Price;
+            TempData["subTypeDescription"] = subscriptionType.Description;
+            return RedirectToAction("Create", "Subscription");
         }
     }
 }
---
+++ 23.1News/Data/Migrations/20231114102336_SubscriptionType.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _23._1News.Data.Migrations
+{
+    public partial class SubscriptionType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Employee",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SubscriptionTypeId",
+                table: "Subscriptions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscriptions_SubscriptionTypeId",
+                table: "Subscriptions",
+                column: "SubscriptionTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Subscriptions_SubscriptionTypes_SubscriptionTypeId",
+                table: "Subscriptions",
+                column: "SubscriptionTypeId",
+                principalTable: "SubscriptionTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Subscriptions_SubscriptionTypes_SubscriptionTypeId",
+                table: "Subscriptions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Subscriptions_SubscriptionTypeId",
+                table: "Subscriptions");
+
+            migrationBuilder.DropColumn(
+                name: "SubscriptionTypeId",
+                table: "Subscriptions");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Employee",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
