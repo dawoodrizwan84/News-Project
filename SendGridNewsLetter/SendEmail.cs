26e7ee2ddead0e826eba9bb183ae00ad97656858
dawoodrizwan84 <dawood.rizwan@outlook.com>
2023-11-27 10:36:45 +0100

    SocketLabs working in queueTrigger

--- SendGridNewsLetter/SendEmail.cs
+++ SendGridNewsLetter/SendEmail.cs
@@ -11,36 +11,37 @@
 using Microsoft.Azure.WebJobs;
 using SendGrid.Helpers.Mail;
 using Microsoft.Extensions.Logging;
-using User = _23._1News.Models.Db.User;
+//using User = _23._1News.Models.Db.User;
 using System.Net.Mail;
 
-namespace SendGridNewsLetter
-{
-    public class SendEmail
-    {
-        [FunctionName("SendEmail")]
-        [return: SendGrid(ApiKey = "23NewsKey", To = "", From = "send.23.1news@outlook.com")]
-        public SendGridMessage Run([QueueTrigger("newsletterqueue", Connection = "AzureWebJobsStorage")] User user, ILogger log)
-        {
-            log.LogInformation($"C# Queue trigger function processed order: {user.Email}");
+//namespace SendGridNewsLetter
+//{
+//    public class SendEmail
+//    {
+//        [FunctionName("SendEmail")]
+//        [return: SendGrid(ApiKey = "23NewsKey", To = "", From = "send.23.1news@outlook.com")]
+//        public SendGridMessage Run([QueueTrigger("newsletterqueue", Connection = "AzureWebJobsStorage")] User user, ILogger log)
+//        {
+//            log.LogInformation($"C# Queue trigger function processed order: {user.Email}");
 
-            SendGridMessage message = new SendGridMessage()
-            {
-                Subject = $"Weekly Newsletter From 23.1News."
-            };
-            message.AddTo(user.Email);
+//            SendGridMessage message = new SendGridMessage()
+//            {
+//                Subject = $"Weekly Newsletter From 23.1News."
+//            };
 
-            message.AddContent("text/html", $"<h3>Good afernoon {user.FirstName}!</h3>" +
-                $"<h4>Articles of your choice.</h4>");
-            return message;
+//            message.AddTo(user.Email);
+
+//message.AddContent("text/html", $"<h3>Good afernoon {user.FirstName}!</h3>" +
+//    $"<h4>Articles of your choice.</h4>");
+//return message;
 
 
-        }
-    }
-    //public class Order
-    //{
-    //    public string OrderId { get; set; }
-    //    public string CustomerName { get; set; }
-    //    public string CustomerEmail { get; set; }
-    //}
-}
+//        }
+//    }
+//public class Order
+//{
+//    public string OrderId { get; set; }
+//    public string CustomerName { get; set; }
+//    public string CustomerEmail { get; set; }
+//}
+//}
