bcd3386ef1bc1c5020052d7893184df7eabb9dac
dawoodrizwan84 <dawood.rizwan@outlook.com>
2023-12-29 09:38:49 +0100

    historical

--- 23.1News/Models/Db/ExchangeRates.cs
+++ 23.1News/Models/Db/ExchangeRates.cs
@@ -1,24 +1,37 @@
-﻿namespace _23._1News.Models.Db
+﻿using Azure;
+using Azure.Data.Tables;
+using Microsoft.Azure.Documents;
+using System.Collections.Concurrent;
+using System.Runtime.Serialization;
+
+
+namespace _23._1News.Models.Db
 {
+
     public class ExchangeRates
     {
+
         public DateTime DateAndTIme { get; set; } = DateTime.Now;
 
         public string Base { get; set; }
 
         public Dictionary<string, decimal> Rates { get; set; }
 
+
     }
+
 
     public class TodaysRate
     {
         public List<ExchangeRates> ExchangeRates { get; set; }
 
-      
     }
 
     public class CurrencyPrice
     {
-        public List<TodaysRate> TodaysRate { get; set;}
+        public List<TodaysRate> todaysRate { get; set; }
     }
+
+
+
 }
--- 23.1News/Services/Abstract/IExchangeRatesService.cs
+++ 23.1News/Services/Abstract/IExchangeRatesService.cs
@@ -5,5 +5,6 @@ namespace _23._1News.Services.Abstract
     public interface IExchangeRatesService
     {
         Task<ExchangeRates> GetRateAsync();
+        Task<List<ExchangeHistoricalEntity>> GetAllHistoricalData(DateTime startDate, DateTime endDate);
     }
 }
